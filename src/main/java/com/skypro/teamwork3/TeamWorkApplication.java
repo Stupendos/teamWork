bdf45c2ccea7e538467d41bd63d1280e401cec83
Stupendos <topakovdenis671@gmail.com>
2025-02-27 20:54:33 +0700

    Start

---
+++ src/main/java/com/skypro/teamwork3/TeamWorkApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.teamwork3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypro/teamwork3/TeamWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.teamwork3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
