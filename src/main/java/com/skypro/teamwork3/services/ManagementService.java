6dc8757c5b71aaba6a79799b1b73e13a449ba927
Stupendos <topakovdenis671@gmail.com>
2025-04-19 20:18:17 +0700

    Подготовка к продакшну, вывод информаци о названии и версии системы

--- src/main/java/com/skypro/teamwork3/services/ManagementService.java
+++ src/main/java/com/skypro/teamwork3/services/ManagementService.java
@@ -2,14 +2,17 @@ package com.skypro.teamwork3.services;
 
 import com.skypro.teamwork3.dto.ServiceInfo;
 import com.skypro.teamwork3.jdbc.repository.RecommendationRepository;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManagementService {
     private final RecommendationRepository recommendationRepository;
+    private final BuildProperties buildProperties;
 
-    public ManagementService(RecommendationRepository recommendationRepository) {
+    public ManagementService(RecommendationRepository recommendationRepository, BuildProperties buildProperties) {
         this.recommendationRepository = recommendationRepository;
+        this.buildProperties = buildProperties;
     }
 
     public void clearCaches() {
@@ -17,6 +20,6 @@ public class ManagementService {
     }
 
     public ServiceInfo getServiceInfo() {
-        return new ServiceInfo("NAME", "VERSION");
+        return new ServiceInfo(buildProperties.getName(), buildProperties.getVersion());
     }
 }
