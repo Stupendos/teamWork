b6a65ac652a9317aa751c08dcef1d88d3b1c1164
IDFCSGFY <idfcsgfy@gmail.com>
2025-04-19 17:26:02 +0700

    base for info request

--- src/main/java/com/skypro/teamwork3/services/ManagementService.java
+++ src/main/java/com/skypro/teamwork3/services/ManagementService.java
@@ -1,19 +1,22 @@
 package com.skypro.teamwork3.services;
 
+import com.skypro.teamwork3.dto.ServiceInfo;
 import com.skypro.teamwork3.jdbc.repository.RecommendationRepository;
-import org.springframework.cache.Cache;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CacheService {
+public class ManagementService {
     private final RecommendationRepository recommendationRepository;
 
-    public CacheService(RecommendationRepository recommendationRepository) {
+    public ManagementService(RecommendationRepository recommendationRepository) {
         this.recommendationRepository = recommendationRepository;
     }
 
-
     public void clearCaches() {
         recommendationRepository.clearCaches();
+    }
+
+    public ServiceInfo getServiceInfo() {
+        return new ServiceInfo("NAME", "VERSION");
     }
 }
